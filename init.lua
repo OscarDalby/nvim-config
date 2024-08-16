2325a45d47ae2f7884a180122f88a481a4c5df94
OscarDalby <dalby.oscar@gmail.com>
2024-08-16 14:05:16 +0100

    init commit of custom nvim config

---
+++ init.lua
@@ -0,0 +1,50 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set clipboard+=unnamedplus")
+vim.cmd("colorscheme torte")
+vim.g.mapleader = " "
+
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  },
+  install = {},
+  checker = { enabled = true },
+}
+)
+
+local builtin_telescope = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin_telescope.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin_telescope.live_grep, {})
+
+local config_treesitter = require("nvim-treesitter.configs")
+config_treesitter.setup({
+  ensure_installed = {"lua", "javascript", "typescript", "go", "python"},
+  highlight = { enable = true },
+  indent = { enable = true },
+}
+)
