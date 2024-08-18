36f4d0f56f7a48e66b814fde98ec54afaca79b75
Oscar Dalby <129061939+OscarDalby@users.noreply.github.com>
2024-08-18 12:38:27 +0100

    added new grep commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,18 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {desc = "open telescope in project scope"})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc = "file grep in project scope"})
+      -- Keymap for finding files in project scope
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "open telescope in project scope" })
+      -- Keymap for grepping in project scope
+      vim.keymap.set("n", "<leader>fp", builtin.live_grep, { desc = "file grep in project scope" })
+      -- Keymap for grepping the current file
+      vim.keymap.set("n", "<leader>ff", function()
+        builtin.grep_string({ search = vim.fn.input("Grep For > ") })
+      end, { desc = "grep current file" })
+      -- Keymap for system-wide grep
+      vim.keymap.set("n", "<leader>fs", function()
+        builtin.live_grep({ search_dirs = { "/" } })
+      end, { desc = "grep system-wide" })
     end,
   },
   {
