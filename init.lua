e72cdd4725b77b19b5a3f658d7acc136f1208744
OscarDalby <dalby.oscar@gmail.com>
2024-08-18 16:13:21 +0100

    added window splits and jumps

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 vim.wo.number = true
@@ -22,15 +22,18 @@ require("lazy").setup("plugins")
 
 -- Format on save
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-  callback = function(args)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = args.buf,
-      callback = function()
-        vim.lsp.buf.format({ async = false, id = args.data.client_id })
-      end,
-    })
-  end,
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(args)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = args.buf,
+			callback = function()
+				vim.lsp.buf.format({ async = false, id = args.data.client_id })
+			end,
+		})
+	end,
 })
 
 vim.api.nvim_create_user_command("RunPy", "silent !python %", {})
+
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "split buffer horizontally" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "split buffer vertically" })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config_treesitter = require("nvim-treesitter.configs")
-    config_treesitter.setup({
-      autoinstall = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-
-    vim.keymap.set("n", "<C-n>", "<Cmd>Neotree toggle<CR>", {desc="toggle Neotree"})
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config_treesitter = require("nvim-treesitter.configs")
+		config_treesitter.setup({
+			autoinstall = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
