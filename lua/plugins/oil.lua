24801b7465b4aecbcbf9a12a75a2e08f4d1db87b
OscarDalby <dalby.oscar@gmail.com>
2024-09-15 09:31:52 +0100

    removed unused plugins

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,4 +5,6 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
+
