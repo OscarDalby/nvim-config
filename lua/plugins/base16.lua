804c6c90a61a94a2b97e2afe2eaa3f6b145131ac
OscarDalby <dalby.oscar@gmail.com>
2024-11-25 00:29:22 +0000

    reverted to torte with new lualine theme

--- lua/plugins/base16.lua
+++ lua/plugins/base16.lua
@@ -2,23 +2,27 @@ return {
   "RRethy/base16-nvim",
   priority = 999,
   config = function()
-    require("base16-colorscheme").setup({
-      base00 = "#000000",
-      base01 = "#454545",
-      base02 = "#3e4451",
-      base03 = "#6c7891",
-      base04 = "#565c64",
-      base05 = "#abb2bf",
-      base06 = "#9a9bb3",
-      base07 = "#c5c8e6",
-      base08 = "#fc0303",
-      base09 = "#ffab61",
-      base0A = "#e5c07b",
-      base0B = "#6bcf23",
-      base0C = "#6190ff",
-      base0D = "#2847fa",
-      base0E = "#bb00c2",
-      base0F = "#a06949",
-    })
+    -- Use below for fully custom colorscheme
+    -- require("base16-colorscheme").setup({
+    --   base00 = "#000000",
+    --   base01 = "#454545",
+    --   base02 = "#3e4451",
+    --   base03 = "#6c7891",
+    --   base04 = "#565c64",
+    --   base05 = "#abb2bf",
+    --   base06 = "#9a9bb3",
+    --   base07 = "#c5c8e6",
+    --   base08 = "#fc0303",
+    --   base09 = "#ffab61",
+    --   base0A = "#e5c07b",
+    --   base0B = "#6bcf23",
+    --   base0C = "#6190ff",
+    --   base0D = "#2847fa",
+    --   base0E = "#bb00c2",
+    --   base0F = "#a06949",
+    -- })
+
+    -- Use below for base16 themes
+    -- vim.cmd("colorscheme  base16-windows-highcontrast")
   end,
 }
