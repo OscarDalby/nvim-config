c906a00abf629ebe909b61213615e7244c2b4d7d
Oscar Dalby <133120805+oscarDalbyRoute101@users.noreply.github.com>
2025-01-10 14:07:42 +0000

    js lsp working finally again

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,4 +1,5 @@
 return {
+  -- Mason
   {
     "williamboman/mason.nvim",
     config = function()
@@ -21,34 +22,74 @@ return {
           "pico8_ls",
           "pyright",
           "grammarly",
+          "ts_ls", -- Add TypeScript/React
         },
       })
     end,
   },
+  -- LSP config
   {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
+
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          client.server_capabilities.documentFormattingProvider = false -- Use null-ls for formatting
+          -- Keymaps
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Documentation" })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+        end,
+      })
+
+      -- Other servers (e.g., pyright, lua_ls, etc.)
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.pyright.setup({ capabilities = capabilities })
-      lspconfig.gopls.setup({ capabilities = capabilities })
-      lspconfig.html.setup({ capabilities = capabilities })
-      lspconfig.tailwindcss.setup({ capabilities = capabilities })
-      lspconfig.pico8_ls.setup({ capabilities = capabilities })
-      lspconfig.marksman.setup({ capabilities = capabilities })
-      lspconfig.dockerls.setup({ capabilities = capabilities })
-      lspconfig.bashls.setup({ capabilities = capabilities })
-      lspconfig.grammarly.setup({ capabilities = capabilities })
-      lspconfig.spectral.setup({ capabilities = capabilities })
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "see information" })
-      vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "go to definition" })
-      vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, { desc = "rename all instances of symbol" })
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "find all references to" })
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "perform code action" })
+      -- Add others as needed...
     end,
   },
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-e>"] = cmp.mapping.abort(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+  -- Add filetype for JSX/TSX
+  vim.filetype.add({
+    extension = {
+      jsx = "javascriptreact",
+      tsx = "typescriptreact",
+    },
+  }),
 }
